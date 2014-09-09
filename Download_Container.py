9070475385bbfc4881386f8eb13771b38ee8e638
Aaron <asmith.technology@gmail.com>
2014-09-09 17:45:35 -0500

    documentation

--- Download_Container.py
+++ Download_Container.py
@@ -2,17 +2,36 @@
 # -*- coding: utf-8 -*-
 #Copyright 2014 Aaron Smith
 
+#    DEPENDENCY:  Pyrax   -->  https://github.com/rackspace/pyrax
+
 #Use this script to download all files in a container.  It will maintain the directory structure and
 #will skip the 0-Byte directory marker files that would cause script to crash.  Using Cyberduck, for
 #example, will cause these 0-byte directory markers to be created and cause an issue with container
-#download.  This script mitigates that issue.
+#download when using tools such as Turbolift or Swiftly.  These are great tools to use, however
+#currently they have trouble with the directory marker object which can be traced to an issue with
+#the multiprocessing module and the order in which objects get downloaded.  This script mitigates that issue.
+#Creates the target cloud container in the current working directory and downloads files into it.
+#You can define the number of processes downloading at once (n_processes).  If you are downloading to a
+#cloud server in the same datacenter as your cloudfiles then you can set "isPublic" to False and make
+#this script use the 'service-net' to download.  You can set the Pyrax module to "debug" output by setting
+#'hDebug' to False.
 
-#Creates the cloud container in the current working directory and downloads files into it.
+##############################  YOU CAN EDIT THE VARIABLES BELOW THIS LINE     ################################
+#Set hDebug to 'True' to debug HTTP calls and see all output.  Set dDebug to 'Flase' so turn off debug output.
+hDebug = False
+#This will defind the number of threads/processes to spawn
+n_processes = 20
+#Set to 'True' to use public network, set to 'False' to use service-net
+isPublic = True
 
-#dependency:  Pyrax   -->  https://github.com/rackspace/pyrax
 
-
-import pyrax
+##############################  DO NOT EDIT BELOW THIS LINE     ################################
+try:
+	import pyrax
+except ImportError as e:
+	print e
+	print "\n"
+	print "Please install Pyrax from 'https://github.com/rackspace/pyrax'."
 import os
 import sys
 import multiprocessing
@@ -21,13 +40,6 @@ username = raw_input('Please enter your USERNAME: ')
 api_key = raw_input('Please enter your APIKEY: ')
 my_region = raw_input("Please enter the container REGION: ")
 my_container = raw_input("Please enter the name of the CONTAINER: ")
-
-#Set hDebug to 'True' to debug HTTP calls and see all output.  Set dDebug to 'Flase' so turn off debug output.
-hDebug = False
-#This will defind the number of threads/processes to spawn
-n_processes = 20
-#Set to 'True' to use public network, set to 'False' to use service-net
-isPublic = True
 
 #Set identity
 pyrax.set_setting('identity_type', 'rackspace')
